5736168c1210cfe125a8bd11b0ffbdc0674a4b5e
Laura Trager <laura.trager@gmail.com>
2017-10-04 17:06:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,4 @@
-board = ["O", " ", " ", "X", " ", " ", " ", "X", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   counter = 0
@@ -10,29 +10,3 @@ def turn_count(board)
   end
   return counter
 end
-
-
-#while counter <=9
-
-#
-
-
-
-
-
-
-#while counter <= 9 do
-#end
-
-
-
-#dependent on whether X or O on board
-#how many spaces are occupied at present
-#need #each to run through the array to check if empty
-#desired return count is turns that have been played = spaces that are not empty
-#use counter to keep track of turns that will iterate over elements check to see if element is X or O, and if it is, increase the counter by 1
-
-
-#puts "Turn number #{counter}"
-#end
-#while counter <= 9
